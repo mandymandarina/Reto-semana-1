fd4f791de722727952c4a956c3d3c4822d7a4db6
Miranda Naranjo <m.naranjo.medel@gmail.com>
2018-05-18 12:10:25 -0400

    se quito prompt de nombre y se coloco un boton

---
+++ app.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    
+    document.getElementById("entrar").addEventListener("click", myFunction);
+    funcion myFuntion() {
+        document.getElementById("entrar").innerHTML = myFunction + " Bienvenido<br>";
+    }
+    
+   
+    var wantToPlay = prompt("¿quieres jugar? s/n");
+    if (wantToPlay.toLowerCase() == "s") {
+        var answer1 = prompt("¿han egresado hombres en Laboratoria? s/n");
+        if (answer1.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No han egresado hombres</p>"
+            "</div>";
+        }
+
+        var answer2 = prompt("¿hay laboratoria en concepción? s/n");
+        if (answer2.toLowerCase() == "n") {
+            document.getElementById("rightAnswersText").innerHTML =
+                document.getElementById("rightAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        } else {
+            document.getElementById("wrongAnswersText").innerHTML =
+                document.getElementById("wrongAnswersText").innerHTML +
+                "<div class='answer'>" +
+                "<p>No hay laboratoria en concepción</p>"
+            "</div>";
+        }
+    } else {
+        document.getElementById("warningMessage").innerText = "Bueno Chao";
+    }
+}
